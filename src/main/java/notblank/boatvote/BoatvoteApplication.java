c38af2d030b6de453ff5efb2f9ab10a7034ab98b
hyeyeon <shymirr20@gmail.com>
2024-09-20 23:31:22 +0900

    init

---
+++ src/main/java/notblank/boatvote/BoatvoteApplication.java
@@ -0,0 +1,13 @@
+package notblank.boatvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notblank/boatvote/BoatvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package notblank.boatvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
