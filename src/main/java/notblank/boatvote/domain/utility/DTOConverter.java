b4e099399e4bd6958a7fa89c8bc5c1c935facbe9
mintuchel <max3627@naver.com>
2024-10-30 23:21:17 +0900

    update: dtoconverter

--- src/main/java/notblank/boatvote/domain/utility/DTOConverter.java
+++ src/main/java/notblank/boatvote/domain/utility/DTOConverter.java
@@ -7,15 +7,42 @@ import notblank.boatvote.domain.survey.dto.internal.FilterDTO;
 import notblank.boatvote.domain.survey.dto.internal.OptionDTO;
 import notblank.boatvote.domain.survey.dto.internal.QuestionDTO;
 import notblank.boatvote.domain.survey.dto.internal.SurveyInfoDTO;
+import notblank.boatvote.domain.survey.dto.request.CreateSurveyRequest;
 import notblank.boatvote.domain.survey.dto.response.SurveyResponse;
 import notblank.boatvote.domain.survey.entity.Survey;
+import notblank.boatvote.domain.user.entity.User;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class DTOConverter {
 
     private final CodeConverter codeConverter;
+
+    public Survey toSurveyEntity(CreateSurveyRequest createSurveyRequest, User owner){
+
+        SurveyInfoDTO surveyInfoDTO = createSurveyRequest.surveyInfo();
+        FilterDTO filters = createSurveyRequest.filters();
+
+        return Survey.builder()
+                .owner(owner)
+                .title(surveyInfoDTO.title())
+                .description(surveyInfoDTO.description())
+                .imgUrl(surveyInfoDTO.imgUrl())
+                .duration(surveyInfoDTO.duration())
+                .maxHeadCnt(surveyInfoDTO.maxHeadCnt())
+                .currentHeadCnt(0)
+                .regionCode(codeConverter.convertRegionListToRegionCode(filters.regionList()))
+                .jobCode(codeConverter.convertJobListToJobCode(filters.jobList()))
+                .ageCode(codeConverter.convertAgeListToAgeCode(filters.ageList()))
+                .genderCode(codeConverter.convertGenderListToGenderCode(filters.genderList()))
+                .point(100) // 포인트는 우리가 알아서 넣어줘야함
+                .questionList(getQuestionList(createSurveyRequest.questions()))
+                .build();
+    }
 
     public SurveyResponse toGetSurveyResponse(Survey survey) {
         return SurveyResponse.builder()
@@ -39,6 +66,7 @@ public class DTOConverter {
     public SurveyInfoDTO toSurveyDTO(Survey survey) {
         return SurveyInfoDTO.builder()
                 .sid(survey.getId())
+                .uid(survey.getOwner().getId())
                 .nickName(survey.getOwner().getNickName())
                 .title(survey.getTitle())
                 .description(survey.getDescription())
@@ -68,5 +96,34 @@ public class DTOConverter {
         return OptionDTO.builder()
                 .text(option.getText())
                 .build();
+    }
+
+    // Survey 에 집어넣을 QuestionList return
+    private List<Question> getQuestionList(List<QuestionDTO> questionDTOList){
+        List<Question> questionList = new ArrayList<>();
+        for(QuestionDTO questionDTO : questionDTOList){
+            Question curQuestion = Question.builder()
+                    .title(questionDTO.title())
+                    .isMultipleAnswer(questionDTO.isMultipleAnswer())
+                    .type(questionDTO.questionType())
+                    .build();
+
+            curQuestion.getOptionList().addAll(getOptionList(questionDTO.optionList()));
+
+            questionList.add(curQuestion);
+        }
+        return questionList;
+    }
+
+    // Question 에 집어넣을 OptionList return
+    private List<Option> getOptionList(List<OptionDTO> optionDTOList){
+        List<Option> optionList = new ArrayList<>();
+        for (OptionDTO curOptionDTO : optionDTOList) {
+            Option option = Option.builder()
+                    .text(curOptionDTO.text())
+                    .build();
+            optionList.add(option);
+        }
+        return optionList;
     }
 }
