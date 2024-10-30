6287bb8afa5d729e0ca9735a11cbb356b2db670a
mintuchel <max3627@naver.com>
2024-10-30 17:52:56 +0900

    update: errorcode

--- src/main/java/notblank/boatvote/global/exception/errorcode/SurveyErrorCode.java
+++ src/main/java/notblank/boatvote/global/exception/errorcode/SurveyErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SurveyErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 설문을 찾을 수 없습니다");
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 질문을 찾을 수 없습니다"),
+    SURVEY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 설문을 찾을 수 없습니다");
 
     private final HttpStatus status;
     private final String message;
