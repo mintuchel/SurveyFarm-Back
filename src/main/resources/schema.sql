2080e8bf4462ffcf1749ad182903ad1ffb4472aa
mintuchel <max3627@naver.com>
2024-11-04 22:34:46 +0900

    refactor: tried to add event scheduler for deadline_survey in schema.sql but failed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,3 +21,45 @@ CREATE TABLE IF NOT EXISTS gendercodes (
     name VARCHAR(255),
     code INT PRIMARY KEY
 );
+
+-- CREATE TABLE IF NOT EXISTS deadline_surveys (
+--     id INTEGER NOT NULL AUTO_INCREMENT,
+--     sid INTEGER,
+--     days_left INTEGER,
+--     PRIMARY KEY (id)
+-- ) engine = InnoDB;
+--
+-- -- MYSQL에 직접 등록하는 마감임박설문 event scheduler
+-- -- CLI 에서는 DELIMETER 통해서 써야하지만
+-- -- schema.sql 에서는 블록단위로 취급하기 때문에 이렇게 써도 ㄱㅊ음
+-- CREATE EVENT IF NOT EXISTS update_deadline_surveys
+-- ON SCHEDULE EVERY 1 DAY
+-- STARTS CURRENT_DATE + INTERVAL 1 DAY
+-- DO
+-- BEGIN
+--     DELETE FROM deadline_surveys;
+--
+--     INSERT INTO deadline_surveys (sid, days_left)
+--     SELECT id, DATEDIFF(end_at, CURDATE())
+--     FROM Survey
+--     WHERE DATEDIFF(end_at, CURDATE()) <= 3 AND DATEDIFF(end_at, CURDATE()) >= 0;
+-- END;
+--
+-- -- CLI 용 테스트 코드
+-- DELIMITER //
+--
+-- CREATE EVENT IF NOT EXISTS update_deadline_surveys
+-- ON SCHEDULE EVERY 1 DAY
+-- STARTS CURRENT_DATE + INTERVAL 1 DAY
+-- COMMENT '마감임박설문 매일 자정에 최신화'
+-- DO
+-- BEGIN
+-- DELETE FROM deadline_surveys;
+--
+-- INSERT INTO deadline_surveys (sid, days_left)
+-- SELECT id, DATEDIFF(end_at, CURDATE())
+-- FROM Survey
+-- WHERE DATEDIFF(end_at, CURDATE()) <= 3 AND DATEDIFF(end_at, CURDATE()) >= 0;
+-- END //
+--
+-- DELIMITER ;
