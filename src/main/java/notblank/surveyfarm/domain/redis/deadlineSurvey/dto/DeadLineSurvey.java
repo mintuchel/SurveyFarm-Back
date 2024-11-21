ca349b73319652804b8316a47040283e09bf795b
mintuchel <max3627@naver.com>
2024-11-21 22:05:52 +0900

    add: deadlinesurvey domain

---
+++ src/main/java/notblank/surveyfarm/domain/redis/deadlineSurvey/dto/DeadLineSurvey.java
@@ -0,0 +1,23 @@
+package notblank.surveyfarm.domain.redis.deadlineSurvey.dto;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+// RedisHash 를 통해 Redis에 의해 서버의 인메모리 캐시에 저장될 객체라는 것을 명시
+// 이 객체의 key 이름은 deadLine이고 Redis에 저장되는 시간은 86400초임 즉 하루임
+// timetoLive 시간이 다 지나면 해당 객체는 알아서 캐시에서 삭제됨
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "deadline", timeToLive = 86400)
+public class DeadLineSurvey {
+    @Id
+    private int id;
+
+    private int daysLeft;
+}
---
+++ src/main/java/notblank/surveyfarm/domain/redis/deadlineSurvey/repository/DeadLineSurveyRedisRepository.java
@@ -0,0 +1,9 @@
+package notblank.surveyfarm.domain.redis.deadlineSurvey.repository;
+
+import notblank.surveyfarm.domain.redis.deadlineSurvey.dto.DeadLineSurvey;
+import org.springframework.data.repository.CrudRepository;
+
+// DeadLineSurvey 자체가 @RedisHash 로 정의되어 Redis 에서 사용됨을 명시하고 있기 때문에
+// 이 Repository 로 작업을 하면 자동으로 Redis 를 통해 인메모리 캐시에 저장됨
+public interface DeadLineSurveyRedisRepository extends CrudRepository<DeadLineSurvey, Integer> {
+}
