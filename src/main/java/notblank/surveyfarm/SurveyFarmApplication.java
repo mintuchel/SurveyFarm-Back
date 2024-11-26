f6847aa3082d26ab407f3677d6879e3d33518f46
mintuchel <max3627@naver.com>
2024-11-26 16:33:49 +0900

    add: spring scheduler for surveystatus update

--- src/main/java/notblank/surveyfarm/SurveyFarmApplication.java
+++ src/main/java/notblank/surveyfarm/SurveyFarmApplication.java
@@ -3,8 +3,9 @@ package notblank.surveyfarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class SurveyFarmApplication {
 	public static void main(String[] args) {
---
+++ src/main/java/notblank/surveyfarm/domain/utility/Scheduler.java
@@ -0,0 +1,26 @@
+package notblank.surveyfarm.domain.utility;
+
+import lombok.RequiredArgsConstructor;
+import notblank.surveyfarm.domain.survey.entity.Survey;
+import notblank.surveyfarm.domain.survey.repository.SurveyRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+
+    private final SurveyRepository surveyRepository;
+
+    // UPDATE 작업이므로 Transactional 선언해줘야함
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * *")
+    public void updateSurveyStatus(){
+        List<Survey> surveys = surveyRepository.getInProgressSurveys();
+
+        for(Survey survey : surveys) survey.updateStatus();
+    }
+}
