6298bf1f0100e64f104574304614dc9cbd4bc0ff
mintuchel <max3627@naver.com>
2024-11-27 03:38:32 +0900

    refactor: createsurveyrequest dto

--- src/main/java/notblank/surveyfarm/domain/survey/dto/request/CreateSurveyRequest.java
+++ src/main/java/notblank/surveyfarm/domain/survey/dto/request/CreateSurveyRequest.java
@@ -3,14 +3,14 @@ package notblank.surveyfarm.domain.survey.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import notblank.surveyfarm.domain.survey.dto.internal.FilterDTO;
-import notblank.surveyfarm.domain.survey.dto.internal.QuestionDTO;
-import notblank.surveyfarm.domain.survey.dto.internal.SurveyInfoDTO;
+import notblank.surveyfarm.domain.survey.dto.request.internal.SurveyFilterDTO;
+import notblank.surveyfarm.domain.survey.dto.common.QuestionDTO;
+import notblank.surveyfarm.domain.survey.dto.request.internal.SurveyInfoDTO;
 
 import java.util.List;
 
 public record CreateSurveyRequest(
     @Valid @NotNull SurveyInfoDTO surveyInfo,
-    @Valid @NotNull FilterDTO filters,
+    @Valid @NotNull SurveyFilterDTO filters,
     @Valid @NotNull @Size(min = 1) List<QuestionDTO> questions
 ) { }
